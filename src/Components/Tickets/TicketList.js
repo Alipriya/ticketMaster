1b1bfb528ee5ae66f8a2225850502741cbf24719
Alipriya <tripathy.alipriya94@gmail.com>
2021-05-05 11:25:56 +0530

    Implementing Charts

--- src/Components/Tickets/TicketList.js
+++ src/Components/Tickets/TicketList.js
@@ -3,6 +3,8 @@ import {Link} from 'react-router-dom'
 import {useSelector,useDispatch} from 'react-redux'
 import {setStartDelete,setCloseTicket} from '../../Actions/TicketAction'
 import "../../App.css"
+import Chart from 'react-google-charts'
+
 function TicketList()
 {
     let dispatch=useDispatch()
@@ -19,7 +21,34 @@ const departmentData=useSelector((state)=>{
 const employeesData=useSelector((state)=>{
     return state.employees
 })
-
+const findDepartment = (id) => {
+    return departmentData.find(dept => dept._id == id)
+}
+const pendingTickets = ticketsData.filter(ticket=>!ticket.isResolved)
+        const high = pendingTickets.filter(ticket=>ticket.priority == 'High').length
+        const medium = pendingTickets.filter(ticket=>ticket.priority == 'Medium').length
+        const low = pendingTickets.filter(ticket=>ticket.priority == 'Low').length
+        const data = [
+            ["Priority", "Tickets per Category"],
+            ["High", high],
+            ["Medium",medium],
+            ["Low", low]
+          ]
+        const options = {
+            title: "Ticket Priority",
+            pieHole: 0.4,
+            is3D: true
+          }
+          const data2 = []
+          const Header = ["Departments", "Tickets", { role: "style" }]
+          data2.push(Header)
+              departmentData.map(dept=>{
+                      const temp = []
+                      temp.push(`${dept.name}`)
+                      temp.push(pendingTickets.filter(ticket=>(ticket.department.name? ticket.department.name : findDepartment(ticket.department).name) == dept.name).length)
+                      temp.push("blue")
+                      data2.push(temp)
+              })
     return(<div className="ticketList">
         <h1 style={{fontFamily:"sans-serif"}}>Listing Tickets -{ticketsData.length}</h1>
         <Link to="/addTicket">Add Ticket</Link>
@@ -72,6 +101,25 @@ const employeesData=useSelector((state)=>{
          </tbody>
      
         </table>
+        <h3>Data on Pending Tickets</h3>
+        <Chart
+                    chartType="PieChart"
+                    width="100%"
+                    height="400px"
+                    data={data}
+                    options={options}
+            />
+        <Chart
+                    chartType="Bar"
+                    width="100%"
+                    height="400px"
+                    data={data2}
+                    options={{
+                        chart: {
+                            title: 'Tickets By Department',
+                        }
+                    }}
+                     />
     </div>)
 
 }
